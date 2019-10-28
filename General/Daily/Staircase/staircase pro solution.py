e379ab08dcef79e8225e30536896b83795f09a48
Kristiyan Bonev <k.s.bonev@gmail.com>
2019-10-28 17:00:13 +0200

    Staircase problem

---
+++ General/Daily/Staircase/staircase pro solution.py
@@ -0,0 +1,25 @@
+
+def staircase_1(n):
+    a, b = 1, 2
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return a
+
+def staircase_2(n, X):
+    cache = [0 for _ in range(n + 1)]
+    cache[0] = 1
+    for i in range(1, n + 1):
+        cache[i] += sum(cache[i - x] for x in X if i - x >= 0)
+    return cache[n]
+
+N = 4
+X = {1, 3, 5}
+
+print(staircase_1(N))
+
+print(staircase_2(N, X))
+
+
+
+
+
---
+++ General/Daily/Staircase/staircase solution.py
@@ -0,0 +1,36 @@
+
+def solution(n):
+    if n == 0 or n == 1:
+        return 1
+    nums = []
+    for x in range(1, n+2):
+        nums.append(x)
+    # print(nums)
+    nums[0] = 1
+    nums[1] = 1
+    for i in range(2, n):
+        nums[i] = nums[i-1] + nums[i-2]
+    # print(nums)
+    return nums[n]
+
+N = 4
+print(solution(N))
+
+def solution_X(n, X):
+    if n == 0:
+        return 1
+    nums = []
+    for x in range(1, n + 2):
+        nums.append(x)
+    # print(nums)
+    nums[0] = 1
+    for i in range(1, n+1):
+        total = 0
+        for j in X:
+            if i - j >= 0:
+                total += nums[i - j]
+        nums[i] = total
+    return nums[n]
+
+X = {1, 3, 5}
+print(solution_X(N, X))
