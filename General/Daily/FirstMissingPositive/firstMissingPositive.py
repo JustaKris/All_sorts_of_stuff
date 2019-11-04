5db4b4764953464848810117adb81d392fdb4e60
Kristiyan Bonev <k.s.bonev@gmail.com>
2019-11-04 17:38:55 +0200

    idk stuff

--- General/Daily/FirstMissingPositive/firstMissingPositive.py
+++ General/Daily/FirstMissingPositive/firstMissingPositive.py
@@ -1,10 +1,10 @@
 
-array = [3, 4, -1, 1, 1, 3, 1, 3, 14]
+array = [3, 4, -1, 1, 1, -3, 1, 3, 7, 5, 6]
 
 def solution(an_array):
     dummy = []
     for x in an_array:
-        if x > 0 and x + 1 not in an_array and x != max(an_array):
+        if x > 0 and x + 1 not in an_array:
             dummy.append(x + 1)
     return min(dummy)
 
